27cbd1cb84ee42dc240843615851f2d76509b81c
Simranjit kaur <simranjit.kaur.005x@gmail.com>
2026-03-09 20:10:58 -0700

    fix allowed hosts for deployment

--- blogproj/blogproj/settings.py
+++ blogproj/blogproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c)*fvxj(=x3i-gs4hd=sradq0n&o*)9tl6)x0k8$h30a6h3(qy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
